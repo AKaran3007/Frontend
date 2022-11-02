95064c648d400fa1bfa23d4adcb62b8b6a2222fa
karan <akaran3007@gmail.com>
2022-11-02 16:17:54 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import Mark from "./Components/Mark";
+import Login from './Components/login_component';
+import SignUp from "./Components/signup_component";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 function App() {
   return (
      <>
-     <Mark></Mark>
+     <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Login/>}></Route>
+    <Route path="/register" element={<SignUp/>}></Route>
+    <Route path="/markup" element={<Mark/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  
      </>
     
   );
